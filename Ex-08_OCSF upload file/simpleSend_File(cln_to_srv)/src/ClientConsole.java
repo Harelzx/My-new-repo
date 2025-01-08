e6066decb1851da8a89199603d21f99c599f7a82
Harelzx <harel@localhost>
2025-01-08 13:35:57 +0200

    add alot of outputs

--- Ex-08_OCSF upload file/simpleSend_File(cln_to_srv)/src/ClientConsole.java
+++ Ex-08_OCSF upload file/simpleSend_File(cln_to_srv)/src/ClientConsole.java
@@ -80,6 +80,12 @@ public class ClientConsole implements ChatIF
     {
       System.out.println
         ("Unexpected error while reading from console!");
+      System.out.println
+      ("Unexpected error while reading from console!");
+      System.out.println
+      ("Unexpected error while reading from console!");
+      System.out.println
+      ("Unexpected error while reading from console!");
     }
   }
 
